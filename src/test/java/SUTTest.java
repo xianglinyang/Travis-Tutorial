bcb2235c34bde6e0256da4fd034573d70792c377
Xianglin Yang <yxlyzxl@gmail.com>
2020-06-06 18:47:12 +0800

    delete test

--- src/test/java/SUTTest.java
+++ src/test/java/SUTTest.java
@@ -4,23 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.Test;
 
 public class SUTTest {
-    @Test
-    public void testStringConcatenation_length2_typeI() {
-        String s1 = "a";
-        String s2 = "b";
-        SUT sut = new SUT();
-        int result = sut.stringConcatenation(s1, s2);
-        assertEquals(result, 1);
-    }
-
-    @Test
-    public void testStringConcatenation_length5_typeII() {
-        String s1 = "abc";
-        String s2 = "de";
-        SUT sut = new SUT();
-        int result = sut.stringConcatenation(s1, s2);
-        assertEquals(result, 2);
-    }
+//    @Test
+//    public void testStringConcatenation_length2_typeI() {
+//        String s1 = "a";
+//        String s2 = "b";
+//        SUT sut = new SUT();
+//        int result = sut.stringConcatenation(s1, s2);
+//        assertEquals(result, 1);
+//    }
+//
+//    @Test
+//    public void testStringConcatenation_length5_typeII() {
+//        String s1 = "abc";
+//        String s2 = "de";
+//        SUT sut = new SUT();
+//        int result = sut.stringConcatenation(s1, s2);
+//        assertEquals(result, 2);
+//    }
     @Test (expected=IllegalArgumentException.class)
     public void testStringConcatenation_length45_throwsException() {
     	String s1="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
