946d4f4db167d1523580ed752a48008f75ac3fbd
Cary__Xx <e0140856@u.nus.edu>
2020-06-06 09:11:58 +0800

    Add tests

--- src/test/java/SUTTest.java
+++ src/test/java/SUTTest.java
@@ -1,3 +1,23 @@
-public class SUTTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Test;
+
+public class SUTTest {
+    @Test
+    public void testStringConcatenation_length2_typeI() {
+        String s1 = "a";
+        String s2 = "b";
+        SUT sut = new SUT();
+        int result = sut.stringConcatenation(s1, s2);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testStringConcatenation_length5_typeII() {
+        String s1 = "abc";
+        String s2 = "de";
+        SUT sut = new SUT();
+        int result = sut.stringConcatenation(s1, s2);
+        assertEquals(result, 2);
+    }
 }
